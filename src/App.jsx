88ff7b2028ad52b7c0803bb7ee86bd00f4a7ebe3
bhanudova02 <bhanudova02@gmail.com>
2024-12-29 17:51:12 +0530

    new changes added

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ function App() {
         </button>
         <SearchBar />
       </div>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 w-[90%] md:w-full mx-auto'>
         {listOfRestaurant.map(restInfo =>
           <RestaurantCard key={restInfo.info.id} restData={restInfo} />
         )}
